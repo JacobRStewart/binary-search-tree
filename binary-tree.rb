cbbd7cfb6f5d8e52513f9f1e5da0cd6f37d31a76
Jacob Stewart <stewart.islmi@gmail.com>
2016-12-19 00:00:57 -0800

    Added breadth-first and depth-first search

--- binary-tree.rb
+++ binary-tree.rb
@@ -31,6 +31,28 @@ class Binary_Tree
     end
   end
 
+  def breadth_first_search(number, queue=[@root_node], depth=0)
+    return nil if queue.empty?
+    next_queue = []
+    queue.each do |node|
+      puts "#{node.value} is located at #{node}, at a depth of #{depth}" if number == node.value
+      next_queue << node.left_child if node.left_child
+      next_queue << node.right_child if node.right_child
+    end
+    queue = next_queue
+    breadth_first_search(number, queue, depth+1)
+  end
+
+
+  def depth_first_search(number, current_node=@root_node, depth=0)
+  	puts "#{current_node.value} is located at #{current_node}, at a depth of #{depth}" if number == current_node.value
+  	if number < current_node.value
+  		depth_first_search(number, current_node.left_child, depth+1) unless current_node.left_child.nil?
+  	else number > current_node.value
+  		depth_first_search(number, current_node.right_child, depth+1) unless current_node.right_child.nil?
+  	end
+  end
+
 end
 
 class Node
@@ -46,3 +68,5 @@ ordered_array = [1,2,3,5,8,11,19,23,26,35,42]
 unordered_array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 
 tree = Binary_Tree.new(unordered_array)
+tree.breadth_first_search(324)
+tree.depth_first_search(324)
